6374f4892d8ef19e2546fe55ee5d670ebb3d34d3
mike19952006 <mike19952006@gmail.com>
2018-03-23 18:36:37 +0200

    Создать статический класс для пересчета температур   - из градусов в кельвины,   - из кельвинов в градусы,   - из градусов в форенгейты,   - из фаренгейтов в градусы,

--- Homework16.1/Program.cs
+++ Homework16.1/Program.cs
@@ -6,10 +6,42 @@ using System.Threading.Tasks;
 
 namespace Homework16._1
 {
+    static class Konvertor
+    {
+        public static int GetDegreefromKelvin(int kelvin)
+        {
+            return kelvin - 273;
+        }
+
+        public static int GetKelvinfromDegree(int degree)
+        {
+            return degree + 273;
+        }
+
+        public static double GetDegreefromFar(int far)
+        {
+            return (far - 32) / 1.8;
+        }
+
+        public static double GetFarfromDegree(int degree)
+        {
+            return degree * 1.8 + 32;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            int degree = Konvertor.GetDegreefromKelvin(300);
+            Console.WriteLine("Degree from Kelvin : " + degree);
+            int kelvin = Konvertor.GetKelvinfromDegree(300);
+            Console.WriteLine("Kelvin from Degree : " + kelvin);
+            double degree1 = Konvertor.GetDegreefromFar(300);
+            Console.WriteLine("Degree from Far : " + Math.Round(degree1,2));
+            double far = Konvertor.GetFarfromDegree(300);
+            Console.WriteLine("Far from degree : " + far);
+
         }
     }
 }
